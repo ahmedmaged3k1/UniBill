299e0090a048a1b4bdd3629c79bc32046d7fcc0c
Mohab2000 <mohabahmed20005@gmail.com>
2023-04-20 00:00:28 +0200

    Signup validation done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,31 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl,FormGroup,Validators} from '@angular/forms';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  loginForm : FormGroup ;
+  loginForm: FormGroup;
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
-      email : new FormControl(null,[Validators.required , Validators.email,Validators.min(4)] ),
-      password : new FormControl(null,[Validators.required , Validators.min(4)])
-    })
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email,
+        Validators.min(4),
+      ]),
+      password: new FormControl(null, [Validators.required, Validators.min(4)]),
+    });
   }
-  onSubmit(){
+  onSubmit() {
     console.log(this.loginForm);
 
     this.loginForm.markAllAsTouched();
-    if(this.loginForm.invalid){
-      alert("Please Submit Valid Data")
+    if (this.loginForm.invalid) {
+      alert('Please Submit Valid Data');
     }
-
   }
 }
-
-
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  signupForm: FormGroup;
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      fullName: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      phone: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^\d+$/),
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
+    });
+  }
+  onSubmit() {
+    console.log(this.signupForm);
 
+    this.signupForm.markAllAsTouched();
+    if (this.signupForm.invalid) {
+      alert('Please Submit Valid Data');
+    }
+  }
 }
