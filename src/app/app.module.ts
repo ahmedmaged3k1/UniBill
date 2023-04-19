f1a8e7cee824a4ffba02b96d316e5410cee52bfb
ziad tarek <66745488+ziadtarek112@users.noreply.github.com>
2023-04-19 15:20:40 +0200

    login Validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -22,7 +22,8 @@ import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl,FormGroup,Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  loginForm : FormGroup ;
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email : new FormControl(null,[Validators.required , Validators.email,Validators.min(4)] ),
+      password : new FormControl(null,[Validators.required , Validators.min(4)])
+    })
+  }
+  onSubmit(){
+    console.log(this.loginForm);
+
+    this.loginForm.markAllAsTouched();
+    if(this.loginForm.invalid){
+      alert("Please Submit Valid Data")
+    }
+
+  }
 }
+
+
