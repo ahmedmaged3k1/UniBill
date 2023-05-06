033bbf576876a5a6dd31b1e731d3933f66f791e7
Ahmed Maged <60134186+ahmedmaged3k1@users.noreply.github.com>
2023-05-06 07:03:53 +0300

    add bill admin ( auth error)

--- src/app/Admin/add-bill/add-bill.component.ts
+++ src/app/Admin/add-bill/add-bill.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Bills } from 'src/app/models/Bills';
+import { BillDataService } from 'src/app/shared/dataService/bill-data.service';
 
 @Component({
   selector: 'app-add-bill',
@@ -8,7 +10,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AddBillComponent implements OnInit {
   form: FormGroup;
-
+  newBill : Bills
+  constructor( private billdata: BillDataService){}
   ngOnInit(): void {
     this.form = new FormGroup({
       billType: new FormControl('', Validators.required),
@@ -23,6 +26,34 @@ export class AddBillComponent implements OnInit {
       alert('Please Submit Valid Data');
       return false;
     }
+    this.saveBillData()
+    this.addBill()
     return true;
+  }
+
+  addBill(): void {
+    this.billdata.addBill(this.newBill)
+      .subscribe(() => {
+        console.log('New bill added successfully.');
+        // do any other required operations here
+      }, error => {
+        console.error('Error adding new bill:', error);
+        // handle the error here
+      });
+  }
+  saveBillData() {
+    const randomId = this.getRandomInt(1, 100000).toString()
+    this.newBill = {
+      id: randomId,
+      amount: this.form.get('amount').value,
+      date: this.form.get('date').value,
+      dueDate: this.form.get('dueDate').value,
+      status: "unpaid",
+      type: this.form.get('billType').value,
+     
+    };
+  }
+  getRandomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
